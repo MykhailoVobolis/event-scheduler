fa1da79784f5cf8859707365aaaf1c89bf3491e6
MykhailoVobolis <mvobolis@gmail.com>
2025-01-19 20:58:26 +0200

    mamoizated selector and fixed pagination

--- src/components/EventList/EventList.tsx
+++ src/components/EventList/EventList.tsx
@@ -1,6 +1,8 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { selectFilteredEvents } from "../../redux/filters/selectors";
-import { selectPaginatedEvents } from "../../redux/events/selectors";
+import { selectCurrentPage, selectPaginatedEvents } from "../../redux/events/selectors";
+import { useEffect } from "react";
+import { setPage } from "../../redux/events/slice";
 
 import PaginationBar from "../PaginationBar/PaginationBar";
 import EventItem from "../EventItem/EventItem";
@@ -8,8 +10,17 @@ import EventItem from "../EventItem/EventItem";
 import css from "./EventList.module.css";
 
 export default function EventList() {
+  const dispatch = useDispatch();
+
   const totalEvents = useSelector(selectFilteredEvents);
   const visibleEvents = useSelector(selectPaginatedEvents);
+  const currentPage = useSelector(selectCurrentPage);
+
+  useEffect(() => {
+    if (visibleEvents.length === 0 && currentPage > 1) {
+      dispatch(setPage(currentPage - 1));
+    }
+  }, [visibleEvents, currentPage, dispatch]);
 
   return (
     <>
--- src/redux/events/selectors.ts
+++ src/redux/events/selectors.ts
@@ -1,3 +1,4 @@
+import { createSelector } from "@reduxjs/toolkit";
 import { selectFilteredEvents } from "../filters/selectors";
 import { RootState } from "../store";
 
@@ -7,13 +8,12 @@ export const selectCurrentPage = (state: RootState) => state.events.currentPage;
 
 export const selectItemsPerPage = (state: RootState) => state.events.itemsPerPage;
 
-export const selectPaginatedEvents = (state: RootState) => {
-  const events = selectFilteredEvents(state);
-  const currentPage = selectCurrentPage(state);
-  const itemsPerPage = selectItemsPerPage(state);
+export const selectPaginatedEvents = createSelector(
+  [selectFilteredEvents, selectCurrentPage, selectItemsPerPage],
+  (events, currentPage, itemsPerPage) => {
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
 
-  const startIndex = (currentPage - 1) * itemsPerPage;
-  const endIndex = startIndex + itemsPerPage;
-
-  return events.slice(startIndex, endIndex);
-};
+    return events.slice(startIndex, endIndex);
+  }
+);
