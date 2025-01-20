842a605adb69dc9f6222f96e86fd5078729eba71
MykhailoVobolis <mvobolis@gmail.com>
2025-01-20 10:54:37 +0200

    added sort events of date

--- src/redux/filters/selectors.ts
+++ src/redux/filters/selectors.ts
@@ -6,10 +6,16 @@ export const selectEvents = (state: RootState) => state.events.events;
 export const selectSearchQuery = (state: RootState) => state.filters;
 
 export const selectFilteredEvents = createSelector([selectEvents, selectSearchQuery], (events, filters) => {
-  return events.filter(
+  const filteredEvents = events.filter(
     (event) =>
       event.title.toLowerCase().includes(filters.title.toLowerCase()) &&
       event.date.toLowerCase().includes(filters.date.toLowerCase()) &&
       event.category.toLowerCase().includes(filters.category.toLowerCase())
   );
+
+  return filteredEvents.sort((a, b) => {
+    const dateA = new Date(`${a.date}T${a.startTime}`).getTime();
+    const dateB = new Date(`${b.date}T${b.startTime}`).getTime();
+    return dateA - dateB;
+  });
 });
